962ce17e12648ea88d8ec8a1a5334051274a5ce3
Kalib Hackin <nagui08@hotmail.com>
2020-10-20 18:27:39 -0500

    adding eslint and firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBCNdJAaxZ5iyVNauhe0cgkkKJVezahSiY',
+  authDomain: 'clone-b5e54.firebaseapp.com',
+  databaseURL: 'https://clone-b5e54.firebaseio.com',
+  projectId: 'clone-b5e54',
+  storageBucket: 'clone-b5e54.appspot.com',
+  messagingSenderId: '874555807834',
+  appId: '1:874555807834:web:a81489bed2bd64e4c02063',
+  measurementId: 'G-LL50HHSJC4',
+};
