d3fc9449dad373f36590b5e9db31ecf75c7cca19
Kalib Hackin <nagui08@hotmail.com>
2020-10-22 20:47:52 -0500

    on click function created

--- src/containers/Cart/index.jsx
+++ src/containers/Cart/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CartItems from '../../components/CartItems';
 import Subtotal from '../../components/Subtotal';
 import './index.scss';
 
@@ -8,6 +9,7 @@ const Cart = () => (
       <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="banner" />
       <div className="info-container">
         <h2 className="info-title">Your Shopping Cart</h2>
+        <CartItems />
       </div>
     </div>
     <div className="right-side">
--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-case-declarations */
 export const initialState = {
   cart: [],
 };
@@ -10,6 +11,21 @@ const reducer = (state, action) => {
       return {
         ...state,
         cart: [...state.cart, action.item],
+      };
+    case 'REMOVE_FROM_CART':
+      const index = state.cart.findIndex(
+        cartItem => cartItem.id === action.id,
+      );
+
+      const newCart = [...state.cart];
+      if (index >= 0) {
+        newCart.splice(index, 1);
+      } else {
+        console.warn(`Can't remove product (id: ${action.id}) as it's not in cart!`);
+      }
+      return {
+        ...state,
+        cart: newCart,
       };
     default: return state;
   }
