2d4bc898bd5b1b44c937c9214ec01128703da809
Kalib Hackin <nagui08@hotmail.com>
2020-10-22 23:00:44 -0500

    creating the html for the login page

---
+++ src/containers/Login/index.jsx
@@ -0,0 +1,29 @@
+import { Link } from '@material-ui/core';
+import React from 'react';
+import './index.scss';
+
+const Login = () => (
+  <div className="login">
+    <Link to="/">
+      <img src="http://ipiccy.com/blog/wp-content/uploads/2016/10/Amazon_logo_plain.svg_.png" alt="Amazon" className="login-logo" />
+    </Link>
+    <div className="login-container">
+      <h1>Sign in</h1>
+      <form>
+        <label htmlFor="email">
+          <h5>E-mail</h5>
+          <input type="text" name="email" id="email" />
+        </label>
+        <label htmlFor="password">
+          <h5>Password</h5>
+          <input type="password" name="password" id="password" />
+        </label>
+        <button type="submit" className="login-button">Sign In</button>
+      </form>
+      <p>By continuing, you agree to Amazon's Fake Clone Conditions of Use and Privacy Notice.</p>
+    </div>
+    <button type="button" className="register-button">Create your Amazon Account</button>
+  </div>
+);
+
+export default Login;
